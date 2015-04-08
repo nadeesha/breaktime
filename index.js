30850f933e4c883cf90cf9fca373637ad1de08df
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2015-04-08 11:30:28 +0530

    Immediate locking fix, Voice prompt

--- index.js
+++ index.js
@@ -12,29 +12,41 @@ var timeDefined = args.length === 4 && _.isNumber(Number(args[2]));
 var helpRequired = args.length === 3 && args[2] === '--help';
 
 if (timeDefined) {
-	exec('clear');
     var finishingAt = moment().add(Number(args[2]), args[3]);
-    startTimer(finishingAt);
+    var fiveSecondsLater = moment().add(5, 'seconds');
+
+    if (finishingAt.isBefore(fiveSecondsLater)) {
+        console.log('I think you gave me an invalid timeframe there...');
+    } else {
+        exec('clear');
+        startTimer(finishingAt);
+    }
 } else if (helpRequired) {
-	printHelp();
+    printHelp();
 } else {
-	exec('clear');
+    exec('clear');
     var finishingAt = moment().add(45, 'minutes');
     startTimer(finishingAt);
 }
 
-function printHelp () {
-	console.log([app.name, app.version, '\n'].join(' '));
+function printHelp() {
+    console.log([app.name, app.version, '\n'].join(' '));
     console.log('usage:   breaktime [amount timeunit]');
     console.log('example: breaktime 45 minutes \n');
     console.log('NOTE: In absence of defined time, \nbreaktime will default to 45 minutes');
 }
 
 function lockScreen() {
-    exec('"/System/Library/CoreServices/Menu Extras/User.menu/Contents/Resources/CGSession" -suspend');
+    exec('say "Locking your screen in 5 seconds"');
+
+    setTimeout(function () {
+        exec('"/System/Library/CoreServices/Menu Extras/User.menu/Contents/Resources/CGSession" -suspend');
+    }, 5000);
 }
 
 function startTimer(endtime) {
+    console.log('I will lock your screen when the timer runs out');
+    console.log('-----------------------------------------------');
     interval = setInterval(function() {
         var minutesToGo = endtime.diff(moment(), 'minutes').toString();
         var secondsToGo = (endtime.diff(moment(), 'seconds') % 60).toString();
